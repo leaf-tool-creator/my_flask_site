d92350d38d8bfbee3e43d8d179127c245f263953
leaf-tool-creator <footapple.aspirin597@passinbox.com>
2025-12-12 13:18:22 -0700

    Fix Render port

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>My Website is Live!</h1>"
+    return "My Flask website is live!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port
+    app.run(host="0.0.0.0", port=port)       # Must use 0.0.0.0
